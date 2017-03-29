90d5de550c44469719c433c92bb3d689b29e9a3c
Emre YILDIRIM <53.emreyildirim@gmail.com>
2017-03-29 22:19:28 +0300

    Naive Bayes Algorithm

---
+++ Naive Bayes Classification/Naive Bayes Classification/Naive Bayes Classification/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Naive_Bayes_Classification
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.model = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.dataView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dosyaYolu = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.countDataList = new System.Windows.Forms.ListBox();
+            this.infoDataList = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // model
+            // 
+            this.model.Enabled = false;
+            this.model.Location = new System.Drawing.Point(460, 315);
+            this.model.Name = "model";
+            this.model.Size = new System.Drawing.Size(116, 148);
+            this.model.TabIndex = 13;
+            this.model.Text = "Model Kur";
+            this.model.UseVisualStyleBackColor = true;
+            this.model.Click += new System.EventHandler(this.model_Click);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(484, 4);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(91, 23);
+            this.buttonOpen.TabIndex = 11;
+            this.buttonOpen.Text = "Aç";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // dataView
+            // 
+            this.dataView.AllowUserToAddRows = false;
+            this.dataView.AllowUserToDeleteRows = false;
+            this.dataView.AllowUserToResizeColumns = false;
+            this.dataView.AllowUserToResizeRows = false;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataView.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dataView.EnableHeadersVisualStyles = false;
+            this.dataView.Location = new System.Drawing.Point(12, 32);
+            this.dataView.Name = "dataView";
+            this.dataView.ReadOnly = true;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataView.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataView.Size = new System.Drawing.Size(563, 277);
+            this.dataView.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Dosya Yolu:";
+            // 
+            // dosyaYolu
+            // 
+            this.dosyaYolu.Enabled = false;
+            this.dosyaYolu.Location = new System.Drawing.Point(79, 6);
+            this.dosyaYolu.Name = "dosyaYolu";
+            this.dosyaYolu.Size = new System.Drawing.Size(399, 20);
+            this.dosyaYolu.TabIndex = 8;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // countDataList
+            // 
+            this.countDataList.FormattingEnabled = true;
+            this.countDataList.Location = new System.Drawing.Point(581, 4);
+            this.countDataList.Name = "countDataList";
+            this.countDataList.Size = new System.Drawing.Size(123, 459);
+            this.countDataList.TabIndex = 23;
+            // 
+            // infoDataList
+            // 
+            this.infoDataList.FormattingEnabled = true;
+            this.infoDataList.Location = new System.Drawing.Point(710, 5);
+            this.infoDataList.Name = "infoDataList";
+            this.infoDataList.Size = new System.Drawing.Size(375, 459);
+            this.infoDataList.TabIndex = 24;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 315);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(442, 148);
+            this.groupBox1.TabIndex = 25;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Girdiler";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 468);
+            this.Controls.Add(this.model);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.infoDataList);
+            this.Controls.Add(this.countDataList);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.dataView);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dosyaYolu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Naive Bayes Classification";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button model;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.DataGridView dataView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox dosyaYolu;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox countDataList;
+        private System.Windows.Forms.ListBox infoDataList;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Naive Bayes Classification/Naive Bayes Classification/Naive Bayes Classification/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Core;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Collections;
+using Microsoft.VisualBasic;
+
+namespace Naive_Bayes_Classification
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Excel.Application ExcelUygulama;
+        Excel.Workbook ExcelProje;
+        Excel.Worksheet ExcelSayfa;
+        object Missing = System.Reflection.Missing.Value;
+        Excel.Range ExcelRange;
+        int rowCount = 0;
+        int columnCount = 0;
+        Hashtable attributes = new Hashtable();
+        int countAttribute = 0;
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+
+            this.Width = 595;
+            attributes.Clear();
+            keyAndUniq.Clear();
+            infoDataList.Items.Clear();
+            countDataList.Items.Clear();
+            combos.Clear();
+            inputs.Clear();
+            model.Enabled = true;       
+            DialogResult result = openFileDialog1.ShowDialog();
+            ExcelUygulama = new Excel.Application();
+            if (openFileDialog1.FileName != "openFileDialog1")
+            {
+                ExcelProje = ExcelUygulama.Workbooks.Open(openFileDialog1.FileName);
+                ExcelSayfa = (Excel.Worksheet)ExcelProje.Worksheets.get_Item(1);
+                ExcelRange = ExcelSayfa.UsedRange;
+                ExcelSayfa = (Excel.Worksheet)ExcelUygulama.ActiveSheet;
+                ExcelUygulama.Visible = false;
+                ExcelUygulama.AlertBeforeOverwriting = false;
+                dosyaYolu.Text = openFileDialog1.FileName + " adlı dosya yüklendi.";
+                dosyaYolu.Enabled = false;
+                rowCount = ExcelRange.Rows.Count + 1;
+                columnCount = ExcelRange.Columns.Count + 1;
+
+                String sdbconnection = @"Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + openFileDialog1.FileName + "; Extended Properties='Excel 12.0; TypeGuessRows=0; HDR=YES; IMEX=1'";
+                OleDbConnection dbconnection = new OleDbConnection(sdbconnection);
+                dbconnection.Open();
+                OleDbDataAdapter dbadapter = new OleDbDataAdapter("Select * from [Sayfa1$]", dbconnection);
+                DataTable dtable = new DataTable();
+                dbadapter.Fill(dtable);
+                dataView.DataSource = dtable;
+                
+                for (int i = 1; i < columnCount; i++)
+                {
+                    List<string> attribute = new List<string>();
+                    for (int j = 2; j < rowCount; j++)
+                    {
+                        object hucre = ExcelSayfa.Cells[j, i];
+                        Excel.Range bolge = ExcelSayfa.get_Range(hucre, hucre);
+                        if (bolge.Value2 != null)
+                            attribute.Add(bolge.Value2.ToString());
+                    }
+                    if (!attributes.ContainsKey(i - 1))
+                        attributes.Add(i - 1, attribute);
+                }
+                countAttribute = attributes.Count - 1;
+                create();
+                doldur();
+            }
+        }
+         List<ComboBox> combos=new List<ComboBox>();
+        private void create() {
+            groupBox1.Controls.Clear();
+            int x = 60; int y = -10; int ls = 1; int lx = 0;
+            for (int i = 0; i < dataView.Columns.Count-1; i++)
+            {
+                if (i % 2 == 0 || i == 0)
+                {
+                    y += 30;
+                    x = 60;
+                    lx = x;                           
+                } else {              
+                    x = 270;
+                    lx = x;                             
+                }
+                Label lbl = new Label();
+                Point lblyer = new Point(lx - 50, y + 5);
+                lbl.Location = lblyer;
+                lbl.Width = 80;
+                lbl.Text = dataView.Columns[i].Name.ToString() + " : ";
+                lbl.Name = "lbl" + dataView.Columns[i].Name.ToString();
+                ls++;
+                groupBox1.Controls.Add(lbl);
+                lx += 20;
+                ComboBox cmb = new ComboBox();
+                Point yer = new Point(x + 40, y);
+                cmb.Name = "cmb" + dataView.Columns[i].Name.ToString();
+                cmb.Location = yer;
+                cmb.Width = 100; cmb.Height = 25;
+                this.Controls.Add(cmb);
+                groupBox1.Controls.Add(cmb);
+                x += 60;             
+                combos.Add(cmb);
+            }
+        }
+        private void doldur()
+        {
+            dataView.Refresh();
+            for (int i = 0; i < combos.Count; i++)         
+                    combos[i].Items.Clear();          
+            foreach (DataGridViewRow row in dataView.Rows) { 
+               for(int i=0;i<combos.Count;i++){
+                   if (!combos[i].Items.Contains(row.Cells[i].Value.ToString()))
+                    combos[i].Items.Add(row.Cells[i].Value.ToString());
+               }
+            }           
+        }   
+        int countTotalData;
+        Hashtable keyAndUniq = new Hashtable();
+        List<string> inputs = new List<string>();     
+        private void model_Click(object sender, EventArgs e)
+        {        
+            countDataList.Items.Clear();
+            infoDataList.Items.Clear();
+            inputs.Clear();
+            for (int i = 0; i < combos.Count; i++)
+            {
+                if (combos[i].Text != "")
+                    inputs.Add(combos[i].SelectedItem.ToString());
+                else
+                {
+                    MessageBox.Show("Girdilerden bir veya bir kaçını seçilmedi.");
+                    return;
+                }
+            }
+            this.Width = 1105;
+            if (dosyaYolu.Text != "")
+            {
+                foreach (int key in attributes.Keys)
+                {
+                    List<string> attribute = (List<string>)attributes[key];
+                    Hashtable uniq = countUniq(attribute);
+                    keyAndUniq.Add(key, uniq);//----->> yani (key=1,value={[key=memelideğil,value=13],[key=memeli,value=7]})
+                    countDataList.Items.Add(dataView.Columns[key].Name.ToUpper());
+                    countDataList.Items.Add("---------------------------------------");
+                    foreach (string key2 in uniq.Keys)
+                    {
+                        int uniqCount = (int)uniq[key2];
+                        countDataList.Items.Add(key2 + " = " + uniqCount.ToString());
+                    }
+                    countDataList.Items.Add("---------------------------------------");
+                    countDataList.Items.Add("");
+                    countTotalData = attribute.Count;
+                }
+                infoDataList.Items.Add("Toplam: " + countTotalData + " kayıt var.");
+                infoDataList.Items.Add("----------------------------------------------------------------------------------------------------------------------------------------------------");
+                List<double> classPro = classProbability(keyAndUniq);
+                List<string> classValues = (List<string>)attributes[attributes.Count - 1];
+                attributesProbability(classPro,classValues);
+            }
+            else  MessageBox.Show("Model Kurmadan Önce Veri Setini Seçiniz.");
+            keyAndUniq.Clear();
+        }
+        public void attributesProbability(List<double> classPro,List<string> classValues) {
+            
+            double tempProduct=0;
+            string tempClass="";
+            int say = 0;
+            Hashtable classAttributes = (Hashtable)countUniq((List<string>)attributes[attributes.Count - 1]);
+            foreach (string key in classAttributes.Keys)
+            {
+                infoDataList.Items.Add("P( " + dataView.Columns[dataView.Columns.Count - 1].Name + " = \"" + key + "\" ) =" + (Convert.ToDouble(classAttributes[key]) / countTotalData));
+                infoDataList.Items.Add("");
+                double product=1;
+                for(int k=0;k<attributes.Count-1;k++){
+                    List<string> attribute = (List<string>)attributes[k];
+                    for (int i = 0; i < attribute.Count; i++)
+                     if (inputs[k] == attribute[i] && key==classValues[i]) 
+                            say++;                                   
+                    product *= (say / Convert.ToDouble(classAttributes[key]));
+                    infoDataList.Items.Add("P( " + dataView.Columns[k].Name + " = \"" + inputs[k] + "\" | " + dataView.Columns[dataView.Columns.Count - 1].Name + " \" "+key+" \") =" + say + " / " + classAttributes[key] + " = " + (say / Convert.ToDouble(classAttributes[key])));
+                    say = 0;
+                }
+                infoDataList.Items.Add("");
+                infoDataList.Items.Add("Çarpımları ="+product);
+                if (tempProduct < product)
+                {
+                    tempProduct = product;
+                    tempClass = key;    
+                }
+
+
+                infoDataList.Items.Add("");
+                infoDataList.Items.Add("P( X | " + dataView.Columns[dataView.Columns.Count - 1].Name + "= \"" + key + "\" ) * P( " + dataView.Columns[dataView.Columns.Count - 1].Name + "= \"" + key + "\" ) = " + product * (Convert.ToDouble(classAttributes[key]) / countTotalData));
+                product = 1;
+                infoDataList.Items.Add("----------------------------------------------------------------------------------------------------------------------------------------------------");
+                infoDataList.Items.Add("X 'in Sınıfı => \"" + dataView.Columns[dataView.Columns.Count - 1].Name+"= "+tempClass+"\"");
+            }
+        }
+        public List<double> classProbability(Hashtable keyAndUniq)
+        {
+            List<double> classPro = new List<double>();
+            foreach(int key in keyAndUniq.Keys){
+                Hashtable classes = (Hashtable)keyAndUniq[key];
+                foreach (string key2 in classes.Keys) {
+                    classPro.Add((Convert.ToDouble(classes[key2]) / countTotalData));                  
+                }             
+            }
+            return classPro;
+        }
+
+        //Sınıfların uniq değerleri ve sayısı bulundu
+        public Hashtable countUniq(List<string> sinifList)
+        {
+            Hashtable countSinif = new Hashtable();
+            for (int i = 0; i < sinifList.Count; i++)
+            {
+                if (countSinif.ContainsKey(sinifList[i]))
+                    countSinif[sinifList[i]] = (Convert.ToInt16(countSinif[sinifList[i]]) + 1);
+                if (!countSinif.Contains(sinifList[i]))
+                    countSinif.Add(sinifList[i], 1);
+            }
+            return countSinif;
+        }
+
+        // Görev yönetici uygulamayı (Exell) kill etmek için 
+      
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Exception Occured while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (dosyaYolu.Text != "")
+            {
+                try
+                {
+                    ExcelProje.Close();
+                    ExcelUygulama.Quit();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                releaseObject(ExcelSayfa);
+                releaseObject(ExcelProje);
+                releaseObject(ExcelUygulama);
+
+                foreach (System.Diagnostics.Process p in System.Diagnostics.Process.GetProcesses())
+                {
+                    if (p.ProcessName == "EXCEL")
+                    {
+                        p.Kill();
+                    }
+                }
+            }
+        }
+
+       
+
+          
+    }
+}
