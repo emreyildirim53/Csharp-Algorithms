f2e2c5a424fedb6837df1408c60c5ee72f194818
Emre YILDIRIM <53.emreyildirim@gmail.com>
2017-03-29 23:25:11 +0300

    First Commit

---
+++ ID3_Algorithm/ID3 Algorithm/ID3 Algorithm/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace ID3_Algorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dosyaYolu = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataView = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.model = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dosyaYolu
+            // 
+            this.dosyaYolu.Enabled = false;
+            this.dosyaYolu.Location = new System.Drawing.Point(75, 6);
+            this.dosyaYolu.Name = "dosyaYolu";
+            this.dosyaYolu.Size = new System.Drawing.Size(311, 20);
+            this.dosyaYolu.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Dosya Yolu:";
+            // 
+            // dataView
+            // 
+            this.dataView.AllowUserToAddRows = false;
+            this.dataView.AllowUserToDeleteRows = false;
+            this.dataView.AllowUserToResizeColumns = false;
+            this.dataView.AllowUserToResizeRows = false;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            this.dataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataView.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dataView.EnableHeadersVisualStyles = false;
+            this.dataView.Location = new System.Drawing.Point(8, 32);
+            this.dataView.Name = "dataView";
+            this.dataView.ReadOnly = true;
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataView.RowHeadersDefaultCellStyle = dataGridViewCellStyle8;
+            this.dataView.Size = new System.Drawing.Size(563, 277);
+            this.dataView.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(392, 4);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(91, 23);
+            this.buttonOpen.TabIndex = 4;
+            this.buttonOpen.Text = "Aç";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(5, 315);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(216, 303);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.Tag = "Gain Tablosu";
+            // 
+            // model
+            // 
+            this.model.Location = new System.Drawing.Point(490, 4);
+            this.model.Name = "model";
+            this.model.Size = new System.Drawing.Size(81, 23);
+            this.model.TabIndex = 6;
+            this.model.Text = "Model Kur";
+            this.model.UseVisualStyleBackColor = true;
+            this.model.Click += new System.EventHandler(this.model_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(227, 315);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(344, 303);
+            this.treeView1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 624);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.model);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.dataView);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dosyaYolu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ID3 Algorithm";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox dosyaYolu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataView;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button model;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ ID3_Algorithm/ID3 Algorithm/ID3 Algorithm/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Core;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Collections;
+
+namespace ID3_Algorithm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+   
+      
+
+        Excel.Application ExcelUygulama;
+        Excel.Workbook ExcelProje;
+        Excel.Worksheet ExcelSayfa;
+        object Missing = System.Reflection.Missing.Value;
+        Excel.Range ExcelRange;
+        int rowCount = 0;
+        int columnCount = 0;
+        Hashtable attributes= new Hashtable();
+        int countAttribute=0;
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            ExcelUygulama = new Excel.Application();
+            if (openFileDialog1.FileName != "openFileDialog1")
+            {
+                ExcelProje = ExcelUygulama.Workbooks.Open(openFileDialog1.FileName);
+                ExcelSayfa = (Excel.Worksheet)ExcelProje.Worksheets.get_Item(1);
+                ExcelRange = ExcelSayfa.UsedRange;
+                ExcelSayfa = (Excel.Worksheet)ExcelUygulama.ActiveSheet;
+                ExcelUygulama.Visible = false;
+                ExcelUygulama.AlertBeforeOverwriting = false;          
+                dosyaYolu.Text = openFileDialog1.FileName + " adlı dosya yüklendi.";
+                dosyaYolu.Enabled = false;
+                rowCount = ExcelRange.Rows.Count + 1;
+                columnCount = ExcelRange.Columns.Count + 1;
+            
+                String sdbconnection = @"Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + openFileDialog1.FileName + "; Extended Properties='Excel 12.0; TypeGuessRows=0; HDR=YES; IMEX=1'";
+                OleDbConnection dbconnection = new OleDbConnection(sdbconnection);
+                dbconnection.Open();
+                OleDbDataAdapter dbadapter = new OleDbDataAdapter("Select * from [Sayfa1$]", dbconnection);
+                DataTable dtable = new DataTable();
+                dbadapter.Fill(dtable);
+                dataView.DataSource = dtable;
+                // başka bir dosya okursa hashi temizlesin diye
+                attributes = new Hashtable();
+                listBox1.Items.Clear();
+                listBox1.Items.Add("-----------------------------------------------------------------------");
+                listBox1.Items.Add("--------------------------ENTROPİLER-----------------------------------");
+                listBox1.Items.Add("-----------------------------------------------------------------------");
+                /*
+                    Excell tablosundan değerler sütun bazında çekildi
+                 */
+                for (int i = 1; i < columnCount; i++)
+                {
+                    List<string> attribute = new List<string>();
+                    for (int j = 2; j < rowCount; j++)
+                    {
+                        object hucre = ExcelSayfa.Cells[j, i];
+                        Excel.Range bolge = ExcelSayfa.get_Range(hucre, hucre);
+                        if (bolge.Value2 != null)
+                            attribute.Add(bolge.Value2.ToString());
+                    }
+                    if (!attributes.ContainsKey(i - 1))
+                        attributes.Add(i - 1, attribute);
+                }
+                countAttribute = attributes.Count - 1;
+            }
+        }
+        TreeNode root = null;
+        TreeNode rootonceki = null;
+        int tempkey = 0;
+        private void model_Click(object sender, EventArgs e)
+        {
+           if(attributes.Count>1) {
+                Hashtable gain = new Hashtable();
+                foreach (int key in attributes.Keys)
+                {
+                    if (key != countAttribute)
+                    {
+                        gain.Add(key, sinifOzellikEntropisi(((List<string>)attributes[key]), ((List<string>)attributes[countAttribute])));
+                        listBox1.Items.Add(gain[key]);
+                    }
+                }
+                double tempGain = 0;
+                int tempAttr = 0;
+                foreach (int key in gain.Keys)
+                {
+                    if ((double)gain[key] > tempGain)
+                    {
+                        tempGain = (double)gain[key];
+                        tempAttr = key;
+                    }
+                }             
+                Hashtable parent = countUniq((List<string>)attributes[tempAttr]);
+                List<string> faydaAttr = (List<string>)attributes[tempAttr];
+                List<string> sinif = (List<string>)attributes[countAttribute];       
+                Hashtable ozellikdal = new Hashtable();
+                foreach (DictionaryEntry dictionaryEntry in parent)
+                {                   
+                    Hashtable dal = new Hashtable();
+                    for (int i = 0; i < faydaAttr.Count; i++)
+                    {
+                        if (faydaAttr[i] == dictionaryEntry.Key.ToString())
+                        {
+                            if (!dal.ContainsKey(sinif[i]))
+                                dal.Add(sinif[i], 1);
+                            else if (dal.ContainsKey(sinif[i]))
+                                dal[sinif[i]] = (Convert.ToInt16(dal[sinif[i]]) + 1);
+                        }
+                    }
+                    ozellikdal.Add(dictionaryEntry.Key, dal);
+                }           
+                List<TreeNode> childs = new List<TreeNode>();           
+                foreach (string key in parent.Keys)
+                {
+                    tempkey++;
+                     Hashtable dal = (Hashtable)ozellikdal[key];
+                     TreeNode ekle = new TreeNode("if (" + dataView.Columns[tempAttr].HeaderText + " )=> '" + key.ToString() + "'");
+                     if (root == null)
+                         treeView1.Nodes.Add(ekle);
+                     else                     
+                         if (rootonceki == null)                        
+                             childEkle(root, ekle);                                           
+                         else if (root != rootonceki)                        
+                             childEkle(rootonceki, ekle);
+      
+                    foreach (DictionaryEntry dictionaryEntry in dal)
+                    {
+                        TreeNode child = new TreeNode(dictionaryEntry.Key.ToString());
+                        childs.Add(child);
+                    }
+                    if (childs.Count == 1)
+                    {
+                        for (int i = 0; i < childs.Count; i++)
+                            childEkle(ekle, (TreeNode)childs[i]);                                 
+                        deleteData(faydaAttr,sinif,key,childs[0].ToString(),attributes);
+                      
+                    } else {                          
+                            rootonceki = root;
+                            root = ekle;                                                            
+                    }
+                    childs.Clear();
+                    if (tempkey == parent.Count)
+                    {
+                        rootonceki = null;
+                        tempkey = 0;
+                       
+                    }                  
+                }              
+                listBox1.Items.Add("En Faydalı --" + dataView.Columns[tempAttr].HeaderText.ToUpperInvariant()+"--");
+                listBox1.Items.Add("");
+                attributes.Remove(tempAttr);
+                columnCount--;
+                treeView1.ExpandAll();
+                model.PerformClick();
+            }
+        }
+        static void childEkle(TreeNode parent,TreeNode value) {
+            parent.Nodes.Add(value);
+        }    
+        public void deleteData(List<string> faydaAttr,List<string> sinif,string faydaData,string sinifData,Hashtable attributes) {
+            String[] sinifAdi = sinifData.ToString().Split(' ');
+            int silinecekIndis = -1;
+            for (int j = 0; j <=countAttribute;j++)
+            {
+                if (attributes.ContainsKey(j))
+                {
+                    List<string> sutunlar = (List<string>)attributes[j];
+                    for (int i = 0; i < sutunlar.Count; i++)                                    
+                        if (faydaData == sutunlar[i])                    
+                            silinecekIndis = i-1;                                                    
+                }
+            }
+            for (int j = 0; j <= countAttribute; j++)
+            {
+                if (attributes.ContainsKey(j))
+                {
+                    List<string> sutunlar = (List<string>)attributes[j];
+                    for (int i = 0; i < sutunlar.Count; i++)
+                    {
+                        if ( i == silinecekIndis)
+                        {                 
+                            sutunlar.RemoveAt(i);
+                            i--;
+                        }
+                    }
+                    attributes[j] = sutunlar;
+                }
+            }
+        }
+        public double sinifEntropi(Hashtable sinif, int count)
+        {
+            double result = 0;
+            foreach (string key in sinif.Keys)
+                result += Convert.ToDouble(sinif[key]) / count * Math.Log((Convert.ToDouble(sinif[key])) / count, 2);
+            return result * -1;
+        }
+        public double ozellikEntropisi(Hashtable unqOzellik,Hashtable unqSinif,List<string> ozellik, List<string> sinif)
+        { 
+            Hashtable ozellikdal = new Hashtable();
+            foreach (DictionaryEntry dictionaryEntry in unqOzellik)
+            {
+                Hashtable dal = new Hashtable();
+                for (int i = 0; i < sinif.Count; i++) {
+                    if (ozellik[i] == dictionaryEntry.Key.ToString())
+                    {
+                        if (!dal.ContainsKey(sinif[i]))
+                            dal.Add(sinif[i], 1);
+                        else if (dal.ContainsKey(sinif[i]))
+                            dal[sinif[i]] = (Convert.ToInt16(dal[sinif[i]]) + 1);                              
+                    }
+                }
+                ozellikdal.Add(dictionaryEntry.Key, dal);
+            }                
+            double h = 0,htemp=0;
+            foreach (string key in unqOzellik.Keys) {             
+                    Hashtable dal = (Hashtable)ozellikdal[key];
+                    foreach (string key3 in dal.Keys) {
+                        htemp += ((Convert.ToDouble(dal[key3]) / Convert.ToDouble(unqOzellik[key]))) * Math.Log((Convert.ToDouble(dal[key3]) / Convert.ToDouble(unqOzellik[key])),2);                  
+                    }               
+                    h += Convert.ToDouble(unqOzellik[key]) / ozellik.Count * -1*(htemp);
+                    htemp = 0;
+            }      
+            return h;
+        }
+        public double sinifOzellikEntropisi(List<string> ozellik, List<string> sinif) {
+            Hashtable unqSinif = countUniq(sinif);         
+            double sinifentropi = sinifEntropi(unqSinif,(((List<string>) attributes[countAttribute]).Count));         
+            Hashtable unqOzellik = countUniq(ozellik);    
+            return sinifentropi - ozellikEntropisi(unqOzellik, unqSinif, ozellik, sinif);
+        }
+        public Hashtable countUniq(List<string> sinifList)
+        {
+            Hashtable countSinif = new Hashtable();
+            for (int i = 0; i < sinifList.Count; i++) {              
+                if (countSinif.ContainsKey(sinifList[i]))
+                    countSinif[sinifList[i]] = (Convert.ToInt16(countSinif[sinifList[i]]) + 1);
+                if (!countSinif.Contains(sinifList[i]))
+                    countSinif.Add(sinifList[i], 1);
+            }
+            return countSinif;
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (dosyaYolu.Text != "")
+            {
+                try
+                {
+                    ExcelProje.Close();
+                    ExcelUygulama.Quit();
+
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                releaseObject(ExcelSayfa);
+                releaseObject(ExcelProje);
+                releaseObject(ExcelUygulama);
+
+                foreach (System.Diagnostics.Process p in System.Diagnostics.Process.GetProcesses())
+                {
+                    if (p.ProcessName == "EXCEL")
+                    {
+                        p.Kill();
+                    }
+                }
+            }
+        }
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Exception Occured while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }     
+    }     
+ }
+
